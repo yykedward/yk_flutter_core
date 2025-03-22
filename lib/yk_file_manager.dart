747ee0f3bfcbd7a29bda720d049edbb61a7b9d58
534272374 <534272374@qq.com>
2025-03-22 18:20:49 +0800

    update core

--- lib/yk_file_manager.dart
+++ lib/yk_file_manager.dart
@@ -1,6 +1,77 @@
+import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class YkFileManager {
 
+  static Future<String> getDocumentPath() async {
+    var documentPath = "";
+    if (Platform.isAndroid) {
+      documentPath = (await getExternalStorageDirectory())?.path ?? "";
+    } else if (Platform.isIOS) {
+      documentPath = (await getApplicationDocumentsDirectory()).path;
+    }
+    return documentPath;
+  }
 
+  static Future<bool> creatPath({required String path}) async {
+    try {
+      final dir = await Directory(path);
+
+      if (!dir.existsSync()) {
+        await dir.create();
+      }
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static Future<bool> save({required List<int> bytes, required String filePath}) async {
+
+    try {
+
+      String fileNameBase = path.basename(filePath);
+
+      String fileDirPath = filePath.replaceAll(fileNameBase, "");
+
+      final createPath = await YkFileManager.creatPath(path: fileDirPath);
+      if (!createPath) {
+        return false;
+      }
+
+      final file = await File(filePath);
+
+      if (!file.existsSync()) {
+        final created = await file.create();
+
+        await created.writeAsBytes(bytes);
+      } else {
+        await file.writeAsBytes(bytes);
+      }
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+
+    return false;
+  }
+
+  static Future<List<int>?> getData({required String path}) async {
+
+    try {
+      final docFile = File(path);
+      final docFileEx = await docFile.exists();
+
+      if (docFileEx) {
+        return docFile.readAsBytes();
+      } else {
+        return null;
+      }
+
+    } catch (e) {
+      return null;
+    }
+  }
 }
