cce11bd33322d80f398f2567c72c0d45d399e21a
534272374 <534272374@qq.com>
2025-07-22 17:41:24 +0800

    add export

--- lib/yk_flutter_core.dart
+++ lib/yk_flutter_core.dart
@@ -4,3 +4,6 @@ export 'package:yk_flutter_core/yk_core_extension.dart';
 export 'package:yk_flutter_core/yk_permission.dart';
 export 'package:yk_flutter_core/yk_action_manager.dart';
 export 'package:yk_flutter_core/yk_core_info.dart';
+export 'package:yk_flutter_core/yk_dig_log_util.dart';
+export 'package:yk_flutter_core/yk_notification_center.dart';
+export 'package:yk_flutter_core/yk_widget.dart';
