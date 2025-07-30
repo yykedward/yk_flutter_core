cacaf8f26665d2169975dfcea5537d86e2318195
534272374 <534272374@qq.com>
2025-07-30 10:38:22 +0800

    update core

--- lib/yk_core_extension.dart
+++ lib/yk_core_extension.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:intl/intl.dart';
+import 'dart:convert';
 
 extension YKIntCoreExtension on int {
   String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) {
@@ -19,3 +22,17 @@ extension YkExtensionString on String {
     }
   }
 }
+
+extension YkDynamicExtension on dynamic {
+
+  dynamic deepCopy() {
+    if (this is Map || this is List) {
+      return json.decode(json.encode(this));
+    } else if (this is String) {
+      String newValue = this;
+      return newValue;
+    }
+    return null;
+  }
+}
+
