c2537b75b907feb6b0bf616c7d8079e190e7082b
534272374 <534272374@qq.com>
2025-12-19 14:42:18 +0800

    update remove yk_supabase_manager

--- lib/yk_flutter_core.dart
+++ lib/yk_flutter_core.dart
@@ -6,5 +6,4 @@ export 'package:yk_flutter_core/yk_core_info.dart';
 export 'package:yk_flutter_core/yk_dig_log_util.dart';
 export 'package:yk_flutter_core/yk_notification_center.dart';
 export 'package:yk_flutter_core/yk_widget.dart';
-export 'package:yk_flutter_core/yk_supabase_manager.dart';
 export 'package:yk_flutter_core/yk_log.dart';
