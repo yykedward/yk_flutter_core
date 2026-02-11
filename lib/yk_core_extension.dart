8acee9d19b0dfe30cfc14760a82b75d2e836c691
534272374 <534272374@qq.com>
2026-02-11 16:43:05 +0800

    update core

--- lib/yk_core_extension.dart
+++ lib/yk_core_extension.dart
@@ -3,8 +3,7 @@ import 'dart:convert';
 
 extension YKIntCoreExtension on num {
   String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) =>
-      DateFormat(formatPattern)
-          .format(DateTime.fromMillisecondsSinceEpoch(toInt()));
+      DateTime.fromMillisecondsSinceEpoch(toInt()).formatTime(formatPattern: formatPattern);
 }
 
 extension YkExtensionString on String {
@@ -16,6 +15,10 @@ extension YkExtensionString on String {
       return "";
     }
   }
+}
+
+extension YkExtensionDateTime on DateTime {
+  String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) => DateFormat(formatPattern).format(this);
 }
 
 extension YkDynamicExtension on dynamic {
