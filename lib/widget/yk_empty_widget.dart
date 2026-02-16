dd74d5422bfcd13dc2c086236920367e5ca84931
edward <534272374@qq.com>
2026-02-16 22:07:48 +0800

    update core

--- lib/widget/yk_empty_widget.dart
+++ lib/widget/yk_empty_widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:yk_flutter_core/widget/yk_text_widget.dart';
 import 'package:yk_flutter_core/yk_core_info.dart';
 
 class YkEmptyWidget extends StatelessWidget {
-  final double height;
-  final double width;
-
-  const YkEmptyWidget({super.key, this.height = 50, this.width = 50});
+  const YkEmptyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      height: height,
-      child: Icon(Icons.layers_clear, color: YkCoreInfo.mainColor, size: 38),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.layers_clear, color: YkCoreInfo.mainColor, size: 38),
+        const SizedBox(height: 2),
+        YkTextWidget("暂无数据",
+            style: TextStyle(fontSize: 20, color: Color(0xFFFFFFFF))),
+      ],
     );
   }
 }
--- lib/yk_core_extension.dart
+++ lib/yk_core_extension.dart
@@ -1,24 +1,31 @@
 import 'package:intl/intl.dart';
 import 'dart:convert';
 
+import 'package:yk_flutter_core/yk_log.dart';
+
 extension YKIntCoreExtension on num {
   String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) =>
-      DateTime.fromMillisecondsSinceEpoch(toInt()).formatTime(formatPattern: formatPattern);
+      DateTime.fromMillisecondsSinceEpoch(toInt())
+          .formatTime(formatPattern: formatPattern);
+
+  double get l => toDouble();
 }
 
 extension YkExtensionString on String {
-  String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) {
+  String? formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) {
     try {
       int time = int.parse(this);
       return time.formatTime(formatPattern: formatPattern);
     } catch (e) {
-      return "";
+      YkLog.log(msg: e.toString());
+      return null;
     }
   }
 }
 
 extension YkExtensionDateTime on DateTime {
-  String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) => DateFormat(formatPattern).format(this);
+  String formatTime({String formatPattern = "yyyy-MM-dd HH:mm:ss"}) =>
+      DateFormat(formatPattern).format(this);
 }
 
 extension YkDynamicExtension on dynamic {
